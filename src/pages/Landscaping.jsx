3596813b7a2c278f50c082dea7b7c97082e975a1
ovie osmund igere <oveey@ovies-MacBook-Pro.local>
2024-07-18 03:51:39 +0100

    clean

--- src/pages/Landscaping.jsx
+++ src/pages/Landscaping.jsx
@@ -127,7 +127,7 @@ export const Landscaping = () => {
       <section className="about__header__section">
         <div className="about__inner__con">
           <Nav />
-          <h1 className="about__header__text">Engineering Construction</h1>
+          <h1 className="about__header__text">Landscaping</h1>
         </div>
       </section>
       <section className="text__under__section">
@@ -140,10 +140,9 @@ export const Landscaping = () => {
       <section className="design__two">
         <div className="design__two__innercon">
           <div className="design__two__innercon__left">
-            <h1><span>01</span> Site Mapping and Drafting</h1>
+            <h1><span>01</span> Landscape Design</h1>
             <p>
-            Our site mapping and drafting services lay the foundation for precise project execution and planning. Key features include
-            </p>
+            Landscape design services focus on creating tailored outdoor environments that complement architectural designs and enrich the overall aesthetic appeal of properties. Key features include:            </p>
             <img
               src="https://res.cloudinary.com/dw3vqhvte/image/upload/v1720351364/Team_zbx8gm.png"
               alt=""
@@ -169,9 +168,9 @@ export const Landscaping = () => {
                 </svg>
               </p>{" "}
               <div className="design__text">
-                <h1>Topographic Surveys </h1>
+                <h1>Conceptual Design </h1>
                 <p>
-                Conducting detailed surveys to map terrain and assess site conditions, facilitating accurate project planning and design.
+                Developing innovative and sustainable landscape concepts that harmonize with architectural styles and client preferences.
                 </p>
               </div>
             </div>
@@ -194,9 +193,9 @@ export const Landscaping = () => {
                 </svg>
               </p>{" "}
               <div className="design__text">
-                <h1>Drafting Services</h1>
+                <h1>Plant Selection</h1>
                 <p>
-                Providing detailed architectural and engineering drawings that serve as blueprints for construction, ensuring clarity and precision in project execution.
+                Expert selection of flora and fauna to enhance biodiversity, promote sustainability, and create visually appealing landscapes.
                 </p>
               </div>
             </div>
@@ -219,15 +218,73 @@ export const Landscaping = () => {
                 </svg>
               </p>{" "}
               <div className="design__text">
-                <h1>Space Planning</h1>
+                <h1>Hardscape Design</h1>
                 <p>
-                Optimization of layouts to maximize spatial efficiency and usability, ensuring every area serves its purpose effectively.
+                Integrating durable and aesthetically pleasing hardscape elements such as pathways, patios, and retaining walls to define and enhance outdoor spaces.
+                </p>
+              </div>
+
+              
+            </div>
+            <div className="design__below__image__text">
+              <p>
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="40"
+                  height="40"
+                >
+                  <circle cx="12" cy="12" r="9.75" fill="#E7E7E7" />
+                  <path
+                    fill="#000000"
+                    fill-rule="evenodd"
+                    d="M15.61 10.186a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </p>{" "}
+              <div className="design__text">
+                <h1>Water Features</h1>
+                <p>
+                Designing and integrating water features such as ponds, fountains, and waterfalls to add tranquility and visual interest to outdoor environments.
                 </p>
               </div>
 
               
             </div>
             
+          </div>
+        </div>
+      </section>
+
+      <section className="modal__section">
+        <h1 className="modal__section__heading">
+          See Our Completed Project On Landscaping Designs
+        </h1>
+        <div className="carousel__modal">
+          <div className="app">
+            {images.map((imageSet) => (
+              <div
+                key={imageSet.key}
+                onClick={() => openModal(imageSet.urls)}
+                className="image__con"
+              >
+                {/* <img src={imageSet.urls[0]} alt="" /> */}
+                <div
+                  style={{ backgroundImage: `url(${imageSet.urls[0]})` }}
+                  className="b__img"
+                >
+                  <div className="inside__image__text">
+                    <h1>{imageSet.mainT}</h1>
+                    <p>{imageSet.smallT}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <Modal show={showModal} onClose={closeModal}>
+              {activeImages.length > 0 && <Carousel images={activeImages} />}
+            </Modal>
           </div>
         </div>
       </section>
